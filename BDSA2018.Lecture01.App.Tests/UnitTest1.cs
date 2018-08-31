9ff7d15ec5f10f75bda15baad53768097fb0768a
Rasmus Lystrøm <rasmusl@microsoft.com>
2018-08-31 12:36:43 +0200

    Project struture lecture 1

---
+++ BDSA2018.Lecture01.App.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BDSA2018.Lecture01.App.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ BDSA2018.Lecture01.App/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDSA2018.Lecture01.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
