9c99e7b36037edb6cfaf55d2c7fbddfa4e5a5fea
Rasmus Lystrøm <rasmusl@microsoft.com>
2018-10-05 15:14:04 +0200

    Lecture 07 start

---
+++ BDSA2018.Lecture07.Models/ICharacterRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BDSA2018.Lecture07.Models
+{
+    public interface ICharacterRepository
+    {
+        Task<int> CreateAsync(CharacterCreateUpdateDTO character);
+
+        Task<CharacterDTO> FindAsync(int characterId);
+
+        IQueryable<CharacterDTO> Read();
+
+        Task<bool> UpdateAsync(CharacterCreateUpdateDTO character);
+
+        Task<bool> DeleteAsync(int characterId);
+    }
+}
