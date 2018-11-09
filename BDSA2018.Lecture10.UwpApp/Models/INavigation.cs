9a7d4a7ac23e3cb88f526dd164b9b1ff69ad501f
Rasmus Lystrøm <rasmusl@microsoft.com>
2018-11-09 16:08:29 +0100

    Added BackCommand to interface

--- BDSA2018.Lecture10.UwpApp/Models/INavigation.cs
+++ BDSA2018.Lecture10.UwpApp/Models/INavigation.cs
@@ -4,6 +4,7 @@ namespace BDSA2018.Lecture10.UwpApp
 {
     public interface INavigation
     {
+        ICommand GoBackCommand { get; }
         ICommand GoToCharacterPageCommand { get; }
         ICommand GoToCharactersPageCommand { get; }
         ICommand GoToMainPageCommand { get; }
